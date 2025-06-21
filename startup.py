7aa117d025738a34bbba5a295b27053d0fdf5490
Luna <lunarinoa.b@gmail.com>
2025-06-21 17:49:39 +0800

    Revert "Update startup.py"
    This reverts commit 06fb760e1e630b6289dd135b09638ca14b50e89e.

--- startup.py
+++ startup.py
@@ -4,11 +4,9 @@ st.set_page_config(
     page_title="Log in or create an account to see Adventure Board"
 )
 
+# manually install bcrypt
 import subprocess
-import importlib.util
-
-if importlib.util.find_spec("bcrypt") is None:
-    subprocess.run(["pip", "install", "bcrypt==4.0.1", "--no-cache-dir"])
+installed = subprocess.run(["pip", "install", "bcrypt==4.0.1", "--no-cache-dir"], capture_output=True, text=True)
 
 from db import get_connection
 import sqlite3
