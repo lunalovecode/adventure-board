8a5359c669d9056f597e64a883e3646a97713a3a
Luna <lunarinoa.b@gmail.com>
2025-06-21 17:49:57 +0800

    Revert "Revert "Successfully add the basics of email login""
    This reverts commit 9c576dc846fedf264f59055e8c7c796dfe1d209c.

--- startup.py
+++ startup.py
@@ -33,9 +33,8 @@ with get_connection() as conn:
                     CREATE TABLE IF NOT EXISTS Accounts (
                     name TEXT NOT NULL,
                     username TEXT NOT NULL UNIQUE,
-                    email TEXT UNIQUE,
-                    profile_pic BLOB NOT NULL UNIQUE,
-                    password BLOB NOT NULL);''')
+                    profile_pic BLOB UNIQUE,
+                    password BLOB);''')
         conn.commit()
     
     posts_exist = cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Posts';").fetchone()
